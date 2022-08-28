0194a3e0cf7d22db94dd3666a2b57ab9d6b405b8
Your Name <samaneh.rezaie74@gmail.com>
2022-08-28 09:21:08 +0430

    ngFor and tracBy

--- secondproject/src/app/app.component.ts
+++ secondproject/src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user:string[]=["samaneh","samira"];
+
+  users:any=[
+    {
+      id:1,
+      name1:"samaneh"
+    },
+    {
+      id:2,
+      name1:"samira"
+    }
+  ]
   title = 'secondproject';
+
+  public addUser(){
+    this.users=[
+      {
+        id:1,
+        name1:"samaneh"
+      },
+      {
+        id:2,
+        name1:"samira"
+      },
+      {
+        id:3,
+        name1:"maryam"
+      },
+      {
+        id:4,
+        name1:"sahar"
+      }
+    ]
+
+  }
+
+  public trackByFunc(index:number,el:any){
+    return el.id;
+  }
 }
