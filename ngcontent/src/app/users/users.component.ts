4b926d622c9241fdfc4da69dfa3b2ac20e95d6bf
Your Name <samaneh.rezaie74@gmail.com>
2022-08-28 15:41:36 +0430

    ng-content

--- ngcontent/src/app/users/users.component.ts
+++ ngcontent/src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  @ContentChild('par',{static:true}) par:ElementRef|any;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.par.nativeElement.textContent)
   }
 
 }
