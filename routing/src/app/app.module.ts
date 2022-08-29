df935d818726e5b4ab7e38fc365c10eed1d2d2cb
Your Name <samaneh.rezaie74@gmail.com>
2022-08-29 13:53:23 +0430

    add url for user home and admin component

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import {  RouterModule, Routes } from '@angular/router';
 
+const routers:Routes=[
+  { path : '',component: HomeComponent  },
+  { path: 'users',component: UserComponent},
+  { path: 'admins', component: AdminComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
