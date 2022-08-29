5221ca2e5a38d1df6091ed0701adf5745aa90d0c
Your Name <samaneh.rezaie74@gmail.com>
2022-08-29 14:30:21 +0430

    routing and notfound url

--- routingdynamic/src/app/app-routing.module.ts
+++ routingdynamic/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminsComponent } from './admins/admins.component';
 import { ClientsComponent } from './clients/clients.component';
 import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'/users', component:UsersComponent},
-  {path:'/admins',component:AdminsComponent},
-  {path:'/clients',component:ClientsComponent}
+  {path:'users', component:UsersComponent},
+  {path:'admins',component:AdminsComponent},
+  {path:'clients',component:ClientsComponent},
+  {path:'not-found',component:NotfoundComponent},
+  {path:'**', redirectTo:'not-found'}
+
 
 ];
 
