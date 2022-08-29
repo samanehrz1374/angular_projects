58413b35a13f605e309e865ee31193669feec104
Your Name <samaneh.rezaie74@gmail.com>
2022-08-29 14:03:35 +0430

    add home,notfound,admin,clients,users components

--- routingdynamic/src/app/app.module.ts
+++ routingdynamic/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { ClientsComponent } from './clients/clients.component';
+import { AdminsComponent } from './admins/admins.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ClientsComponent,
+    AdminsComponent,
+    NotfoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
