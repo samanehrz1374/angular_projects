423c18c3c147c7048145c694773969bbeeff6061
Your Name <samaneh.rezaie74@gmail.com>
2022-08-29 14:07:31 +0430

    add url for routing

--- routingdynamic/src/app/app-routing.module.ts
+++ routingdynamic/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminsComponent } from './admins/admins.component';
+import { ClientsComponent } from './clients/clients.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'/users', component:UsersComponent},
+  {path:'/admins',component:AdminsComponent},
+  {path:'/clients',component:ClientsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
