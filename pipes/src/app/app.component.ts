21c7bef1fed9bcf88b3fb2635583c3175f78cf64
Your Name <samaneh.rezaie74@gmail.com>
2022-08-30 17:57:50 +0430

    add pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -10,6 +10,22 @@ export class AppComponent {
   presentDate=new Date();
   title = 'pipes';
 
+  price:number=2000;
+
+  Fruits=["Apple","Orange","Grapes","Mango","Kiwi"];
+
+  decimalNum1:number= 8.7589623;
+  decimalNum2:number= 5.43;
+
+  jsonData = {
+    id:'one',
+    name:{ username: 'user1'}
+  };
+  
+
+
+
+
   timeCahnge = new Observable<string>((observer:Observer<string>)=>{
     setInterval(()=> observer.next(new Date().toString()),1000);
   })
