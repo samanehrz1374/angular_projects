e8ad25b751c1889e092bbc88fa030e787c5e95ca
Your Name <samaneh.rezaie74@gmail.com>
2022-08-30 12:16:05 +0430

    add logging services

---
+++ RoutingQueryParams/src/app/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loggedIn:boolean=false;
+
+  public isAuthenticated(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(this.loggedIn);
+      },1000);
+
+    })
+  }
+
+  public checkAuth(){
+    return this.loggedIn;
+  }
+
+
+
+  public loggin(){
+    this.loggedIn=true;
+  }
+
+  public loggedout(){
+    this.loggedIn=false;
+  }
+
+}
