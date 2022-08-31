e0aa3d32a50912e9183b37ff91336637e068f6b3
Your Name <samaneh.rezaie74@gmail.com>
2022-08-31 14:27:45 +0430

    use http in angular

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  httpdata:any;
   title = 'http';
+
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit() {
+    this.http.get("http://jsonplaceholder.typicode.com/users").
+    subscribe((data) => this.displaydata(data))
+  }
+
+  displaydata(data:any){
+    this.httpdata = data;
+  }
 }
